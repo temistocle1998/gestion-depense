a13b05ee8737d34bd82d4556c7705ed06643c7a6
Lamine Beye <laminebeye007@gmail.com>
2021-09-24 02:57:57 +0000

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/gestion-depense'));
+app.use(jwt());
 
 app.get('/*', function(req,res) {
-    
+
 res.sendFile(path.join(__dirname+'/dist/gestion-depense/index.html'));
 });
 
