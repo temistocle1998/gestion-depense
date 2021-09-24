cf05dc6903c05242a45de9ee1fc462594eb7dd6c
Lamine Beye <laminebeye007@gmail.com>
2021-09-24 03:16:45 +0000

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-//Install express server
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+      return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(requireHTTPS);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gestion-depense'));
-
-
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/gestion-depense/index.html'));
+app.use(express.static('./dist/gestion-depense.json>'));
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/gestion-depense.json>/'}
+);
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
